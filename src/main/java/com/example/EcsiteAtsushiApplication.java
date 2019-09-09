4135f4871e01525f2466fac5d70ef4fcd9603869
aizawaatsushi <atsushi.aizawa@rakus-partners.co.jp>
2019-09-09 12:33:54 +0900

    first commit

---
+++ src/main/java/com/example/EcsiteAtsushiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteAtsushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteAtsushiApplication.class, args);
+	}
+
+}
